28e946bd07fb8b273abb233d2d6620f1d5d9c4ac
valechow <valechowseg@gmail.com>
2024-08-14 10:13:28 -0600

    Inserción tabla Hospital

---
+++ Proyecto_Lenguajes_BD.sql
@@ -0,0 +1,10 @@
+/*Proyecto Final Lenguajes de Base de Datos */
+
+-- Tabla Hospital
+
+CREATE TABLE Hospital (
+    ID_Hospital NUMBER PRIMARY KEY,
+    Nombre_Hospital VARCHAR2(50),
+    Direccion_Hospital VARCHAR2(100),
+    Telefono_Hospital NUMBER
+);
